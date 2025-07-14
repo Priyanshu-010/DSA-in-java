6b1e836fcf668fdb2e4e8268e219e17a9744573c
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-07-14 11:50:26 +0530

    Bubble sort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {7,2,6,7,9,4,5};
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        printArray(arr);
+
+    }
+    static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
