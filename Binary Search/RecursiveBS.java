525dd19487610479c2ca8215016b9efe305f75cc
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-07-23 20:45:57 +0530

    Better way to calculate mid

--- Binary Search/RecursiveBS.java
+++ Binary Search/RecursiveBS.java
@@ -3,13 +3,14 @@ public class RecursiveBS {
         int[] arr = {2,5,7,9,11,15,24};
         int s = 0;
         int end = arr.length-1;
-        int target = 6;
+        int target = 24;
         boolean ans = binarySearch(arr,s, end, target);
         System.out.println(ans);
     }
     static boolean binarySearch(int[] arr, int s, int end, int target){
         if(s >  end) return false;
-        int mid = (s +end)/2;
+        int mid = s +(end - s)/2; // better way to calculate mid
+        // as sometimes mid can go above the value of maximum an integer can save which is 2 raise to 31
         if(target == arr[mid]){
             return true;
         } else if(arr[mid] > target){
--- Binary Search/SimpleBS.java
+++ Binary Search/SimpleBS.java
@@ -1,7 +1,7 @@
 public class SimpleBS {
     public static void main(String[] args) {
         int[] arr = {2,5,7,9,11,15,24};
-        int target = 24;
+        int target = 9;
         int ans = binarySearch(arr, target);
         System.out.println(ans);
     }
@@ -11,7 +11,8 @@ public class SimpleBS {
 
 
         while(s <= end){
-            int mid = (s + end)/2;
+            int mid = s +(end - s)/2; // better way to calculate mid
+            // as sometimes mid can go above the value of maximum an integer can save which is 2 raise to 31
             if(arr[mid] == target){
                 return mid;
             }else if(arr[mid] > target){
