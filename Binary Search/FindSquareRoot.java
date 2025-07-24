d882332e4101656e104495b7ecc6e12526f97f8f
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-07-24 12:01:02 +0530

    Finding square root using binary search

---
+++ Binary Search/FindSquareRoot.java
@@ -0,0 +1,25 @@
+public class FindSquareRoot {
+    public static void main(String[] args) {
+        int x = 16;
+        System.out.println(squareRoot(x));
+    }
+    static int squareRoot(int x){
+        int s = 0, end = x;
+        int ans = -1;
+
+        while (s<= end){
+            int mid = s+(end-s)/2;
+            long val = (long)mid * mid;
+            if(val == x){
+                return mid;
+            } else if (val < x){
+                ans = mid;
+                s = mid + 1;
+            } else {
+                end = mid -  1;
+            }
+        }
+
+        return ans;
+    }
+}
