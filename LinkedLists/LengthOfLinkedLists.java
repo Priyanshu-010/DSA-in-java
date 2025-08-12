7f251edb5073b9a94466180961a0472b3ad95316
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-08-12 13:32:15 +0530

    Finding length of linked lists

--- LinkedLists/LengthOfLinkedLists.java
+++ LinkedLists/LengthOfLinkedLists.java
@@ -1,5 +1,30 @@
 public class LengthOfLinkedLists {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
     public static void main(String[] args) {
-        
+        Node a = new Node(5);
+        Node b = new Node(3);
+        Node c = new Node(9);
+        Node d = new Node(10);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+
+        int len = lenghtOfLL(a);
+        System.out.println(len);
+    }
+    public static int lenghtOfLL(Node head){
+        int len = 0;
+        while(head != null){
+            len++;
+            head = head.next;
+        }
+        return len;
     }
 }
