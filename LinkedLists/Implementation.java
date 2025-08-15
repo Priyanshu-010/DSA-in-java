b67a960262110e9bfa4719d0d22989c551c5d9fe
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-08-15 15:58:48 +0530

    Insert at start function

--- LinkedLists/Implementation.java
+++ LinkedLists/Implementation.java
@@ -14,12 +14,26 @@ public class Implementation {
 
         void insertAtEnd(int x){
             Node temp = new Node(x);
-            if(head == null){
+            if(head == null){ // empty list
                 head = temp;
             }else{
                 tail.next = temp;
             }
             tail = temp;
+        }
+        void insertAtStart(int x){
+            Node temp = new Node(x);
+            if(head == null){ // empty list
+//                head = temp;
+//                tail = temp;
+                // Or
+                head = tail = temp;
+                // Or
+                //insertAtEnd(x); // as the head is null it will only run the if condition of head is null
+            }else{
+                temp.next = head;
+                head = temp;
+            }
         }
         void display(){
             Node temp = head;
@@ -27,12 +41,17 @@ public class Implementation {
                 System.out.print(temp.data + " ");
                 temp = temp.next;
             }
+            System.out.println();
         }
     }
     public static void main(String[] args) {
         Linkedlist ll = new Linkedlist();
         ll.insertAtEnd(5);
         ll.insertAtEnd(7);
+        ll.display();
+        ll.insertAtStart(2);
+        ll.insertAtStart(8);
+        ll.insertAtStart(19);
         ll.display();
     }
 }
