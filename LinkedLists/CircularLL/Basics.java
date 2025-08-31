c9c3c68c9a54e03cdfffae78c45ecefdb04d9304
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-08-31 22:49:14 +0530

    Circular Linked list

---
+++ LinkedLists/CircularLL/Basics.java
@@ -0,0 +1,33 @@
+package CircularLL;
+
+public class Basics {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+        Node a = new Node(5);
+        Node b = new Node(3);
+        Node c = new Node(9);
+        Node d = new Node(10);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = a;
+        display(a);
+    }
+
+    public static void display(Node head){
+        Node temp = head.next;
+        System.out.print(head.data + " ");
+        while(temp != head){
+            System.out.print(temp.data +  " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
