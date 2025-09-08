dbd130d71c6aebe93c7134322475ea7b0c029328
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-09-08 17:31:02 +0530

    Next Greater Element with arrays

---
+++ Stack/Questions/NextGreaterElement.java
@@ -0,0 +1,28 @@
+package Questions;
+
+import java.util.Stack;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,1,8,6,3,4};
+        int[] ans = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = -1;
+            for (int j = i+1; j < arr.length ; j++) {
+                if(arr[j] > arr[i]){
+                    ans[i] = arr[j];
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println();
+    }
+}
