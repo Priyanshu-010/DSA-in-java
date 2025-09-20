d6b10423010cf2af5a92d1ef7042106077579e1d
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-09-20 20:07:54 +0530

    Queue Basics

---
+++ queue/basics/Basics.java
@@ -0,0 +1,23 @@
+package basics;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Basics {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(2);
+        q.add(1);
+        q.add(7);
+        q.add(4);
+        System.out.println(q.size());
+        System.out.println(q);
+        q.remove();
+        System.out.println(q);
+        System.out.println(q.size());
+        q.poll();  // remove
+        System.out.println(q);
+        System.out.println(q.size());
+        System.out.println(q.element()); // peek
+    }
+}
